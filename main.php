18203ac8a9d289eac4a3464b684a658c82e08237
akm1504 <anandu.poly@gmail.com>
2018-07-04 20:38:07 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
     require('telegram-bot-api.php');
 
-    $token = '10**************************************jM';
+    $token = '585060714:AAEzvzzCKloTn3nqCC4o3kS9r1y71FkCYro';
     $bot = new telegram_bot($token);
-    $to = '@myChannel';
+    $to = '@akm1504';
     $rs = $bot->send_message($to , 'test' , null, null);
         print_r($rs);
 ?>
